90c8308aa271b412c8c5c6476b9f8da2f633c4f9
rachelslurs <rachelcantor@gmail.com>
2024-12-01 13:07:27 -0500

    fix: updates

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -12,7 +12,7 @@ export default function WorkCard({
   frontmatter,
   secHeading = true,
 }: Props) {
-  const { title, year, description } = frontmatter;
+  const { title, description } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
